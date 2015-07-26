7b7d473e20ead890ef26f68ef459fe6fb4b81efb
Marc Zottner <Maarc@users.noreply.github.com>
2015-07-26 10:45:54 +0200

    Documentation cleanup

--- bootstrap_03_end.sh
+++ bootstrap_03_end.sh
@@ -7,7 +7,7 @@ search ${DOMAIN_NAME}
 nameserver ${IP_PREFIX}.100
 EOF
 
-# TODO ... check after a full build
+# CHECKME after a full build (done to resolve network issues)
 echo "HWADDR=\"$(ifconfig eth0  | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')\"" >> /etc/sysconfig/network-scripts/ifcfg-eth0
 echo "HWADDR=\"$(ifconfig eth1  | grep -o -E '([[:xdigit:]]{1,2}:){5}[[:xdigit:]]{1,2}')\"" >> /etc/sysconfig/network-scripts/ifcfg-eth1
 systemctl restart network
